1e8dd1cab22f8f4ecf3af0a9e6210da44d733aef
Andrew Lee <aiaechel@gmail.com>
2016-05-31 17:31:17 -0700

    Added migration 2016_05_31: add 'discount_codes' table, modify 'orders' table to contain a 'discount_code_id' column

---
+++ database/migrations/2016_05_31_232051_add_discount_codes.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDiscountCodes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discount_codes', function (Blueprint $table) {
+            $table->increments('id');
+	    $table->enum('type', ['flat', 'percent']);
+	    $table->decimal('amount', 8, 2)->default(0.00);
+	    $table->string('code', 32);
+	    $table->datetime('exp_at')->nullable();
+	    $table->integer('times_used')->unsigned()->default(0);
+	    $table->integer('max_times_used')->unsigned()->default(PHP_INT_MAX);	    
+	    $table->integer('event_id');
+
+	    $table->index(['event_id', 'code']);
+	    $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+        });
+
+	Schema::table('orders', function($table) {
+	    $table->integer('discount_code_id')->nullable();
+	});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	Schema::table('orders', function($table) {
+	    $table->dropColumn('discount_code_id');
+	});
+        Schema::drop('discount_codes');
+    }
+}
