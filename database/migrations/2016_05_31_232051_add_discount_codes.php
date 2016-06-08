2516756633778ecc22220023cdbb7f403a70534f
Andrew Lee <aiaechel@gmail.com>
2016-06-08 16:30:33 -0700

    Added the Create Discount Code modal and modified the migration to add the 'account_id' field

--- database/migrations/2016_05_31_232051_add_discount_codes.php
+++ database/migrations/2016_05_31_232051_add_discount_codes.php
@@ -13,21 +13,26 @@ class AddDiscountCodes extends Migration
     public function up()
     {
         Schema::create('discount_codes', function (Blueprint $table) {
+	    $table->timestamps();
+	    $table->softDeletes();
+
             $table->increments('id');
 	    $table->enum('type', ['flat', 'percent']);
 	    $table->decimal('amount', 8, 2)->default(0.00);
 	    $table->string('code', 32);
 	    $table->datetime('exp_at')->nullable();
 	    $table->integer('times_used')->unsigned()->default(0);
-	    $table->integer('max_times_used')->unsigned()->default(PHP_INT_MAX);	    
-	    $table->integer('event_id');
-
+	    $table->integer('max_times_used')->unsigned()->default(PHP_INT_MAX);
+	    $table->integer('account_id')->unsigned();
+	    $table->integer('event_id')->unsigned();
 	    $table->unique(['event_id', 'code']);
-	    $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
 
-	    $table->timestamps();
-	    $table->softDeletes();
         });
+
+	Schema::table('discount_codes', function($table) {
+	    $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+	    $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+	});
 
 	Schema::table('orders', function($table) {
 	    $table->integer('discount_code_id')->nullable();
